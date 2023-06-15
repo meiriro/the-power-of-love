3ae6e2da316d5eabcf1ea56045b7afc445dd34eb
meiriro <117658512+meiriro@users.noreply.github.com>
2023-06-15 19:42:16 -0300

    Delete js directory

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle")
-const navMenu = document.querySelector(".nav-menu")
-
-navMenu.toggleAttribute.addEventListener("click", () => { 
-    navMenu.classList.toggle("nav-menu_visible");
-});
